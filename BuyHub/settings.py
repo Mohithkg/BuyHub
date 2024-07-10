fd94164b9271cb80784fd73a9bf1a71a25c865eb
Mohithkg <mohithkg931@gmail.com>
2024-07-10 18:21:55 +0530

    commit the changes

--- BuyHub/settings.py
+++ BuyHub/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Category',
+    'Accounts',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BuyHub.wsgi.application'
 
+AUTH_USER_MODEL = 'Accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- Category/models.py
+++ Category/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    """
+       Represents a product category in the e-commerce platform.
+       Attributes:
+           category_name (CharField): The name of the category. This field must be unique.
+           slug (CharField): A URL-friendly version of the category name, used for SEO and routing. This field must be unique.
+           description (TextField): A brief description of the category. This field is optional.
+           category_image (ImageField): An optional image associated with the category. The image is uploaded to 'photos/categories/'.
+       """
+    category_name=models.CharField(max_length=50, unique=True)
+    slug= models.CharField(max_length=100, unique=True)
+    description= models.TextField(max_length=255, blank=True)
+    category_image= models.ImageField(upload_to='photos/categories/', blank=True)
+
+    class Meta:
+        verbose_name= 'Category'
+        verbose_name_plural= 'Categories'
+
+    def __str__(self):
+        return self.category_name

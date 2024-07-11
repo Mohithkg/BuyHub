0e1c668c45acb2235868d27d214221e931915e2a
Mohithkg <mohithkg931@gmail.com>
2024-07-11 19:19:38 +0530

    Added Cart

--- BuyHub/settings.py
+++ BuyHub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Category',
     'Accounts',
     'Store',
+    'Carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'Category.context_processors.menu_links',
+                'Carts.context_processors.counter',
             ],
         },
     },
--- BuyHub/urls.py
+++ BuyHub/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('store/',include('Store.urls')),
+    path('cart/',include('Carts.urls')),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
